a44c01865dddf2b99fd1e0f60e360a25066d2b18
Lucas Leão <107482465+imafancydev@users.noreply.github.com>
2024-12-11 18:26:36 +0000

    feat: funcs added

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: laugusto <laugusto@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 00:17:06 by laugusto          #+#    #+#             */
-/*   Updated: 2024/12/10 23:40:52 by laugusto         ###   ########.fr       */
+/*   Updated: 2024/12/11 18:25:59 by laugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,4 +37,86 @@ ssize_t	ft_strlen(const char *str)
 	while (str[size])
 		size++;
 	return (size);
+}
+
+char	*ft_strjoin(char *line, char *buff)
+{
+	char			*str;
+	size_t			i;
+	size_t			j;
+
+	if (!line)
+	{
+		line = (char *)malloc(sizeof(char) * 1);
+		line[0] = '\0';
+	}
+	if (!line || !buff)
+		return (NULL);
+	str = malloc(sizeof(char) * (ft_strlen(line) + ft_strlen(buff) + 1));
+	if (str == NULL)
+		return (NULL);
+	i = -1;
+	j = 0;
+	if (line)
+		while (line[++i] != '\0')
+			str[i] = line[i];
+	while (buff[j] != '\0')
+		str[i++] = buff[j++];
+	str[ft_strlen(line) + ft_strlen(buff)] = '\0';
+	free(line);
+	return (str);
+}
+
+char	*ft_line_to_buff(char *line)
+{
+	int		i;
+	char	*s;
+
+	i = 0;
+	if (!line[i])
+		return (NULL);
+	while (line[i] && line[i] != '\n')
+		i++;
+	s = (char *)malloc(sizeof(char) * (i + 2));
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (line[i] && line[i] != '\n')
+	{
+		s[i] = line[i];
+		i++;
+	}
+	if (line[i] == '\n')
+	{
+		s[i] = line[i];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
+
+char	*ft_read_new_line(char *line)
+{
+	int		j;
+	int		i;
+	char	*s;
+
+	i = 0;
+	while (line[i] && line[i] != '\n')
+		i++;
+	if (!line[i])
+	{
+		free (line);
+		return (NULL);
+	}
+	s = (char *)malloc(sizeof(char) * (ft_strlen(line) - i + 1));
+	if (!s)
+		return (NULL);
+	i++;
+	j = 0;
+	while (line [i])
+		s[j++] = line[i++];
+	s[j] = '\0';
+	free(line);
+	return (s);
 }
